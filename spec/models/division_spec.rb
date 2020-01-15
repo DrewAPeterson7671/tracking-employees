60853360956169aa1db0fbebbc84f0e6229ee1d2
Student <example@example.com>
2020-01-15 15:37:17 -0800

    add callback

--- spec/models/division_spec.rb
+++ spec/models/division_spec.rb
@@ -5,3 +5,10 @@ describe Division do
   it { should validate_presence_of (:div_name) }
   it { should validate_length_of(:div_name).is_at_most(30) }
 end
+
+describe Division do
+  it("titleizes the name of an division") do
+    division = Division.create({div_name: "giant steps"})
+    expect(division.div_name()).to(eq("Giant Steps"))
+  end
+end
