f95ef9977390066360e26589e1e221b2166c2be3
Student <example@example.com>
2020-01-15 15:31:12 -0800

    add validations

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,3 +1,5 @@
 class Division < ApplicationRecord
   has_many :employees, dependent: :destroy
+  validates :div_name, presence: true
+  validates_length_of :div_name, maximum: 30
 end
--- spec/models/division_spec.rb
+++ spec/models/division_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Division do
   it { should have_many(:employees) }
+  it { should validate_presence_of (:div_name) }
+  it { should validate_length_of(:div_name).is_at_most(30) }
 end
